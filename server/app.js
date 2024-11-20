a4e99ace83794cf7981b1c9d8d5c5e0288bf9103
Jithu Prabhakar <join.jitz@gmail.com>
2024-11-20 20:24:28 +0530

    completes the app

--- server/app.js
+++ server/app.js
@@ -1,18 +1,21 @@
-const express = require("express");
-require("./db/connection");
-const auth = require("./routes/auth");
-const todo = require("./routes/todo");
+const express = require('express')
+require('./db/connection')
+const auth = require('./routes/auth')
+const todo = require('./routes/todo')
 
-const app = express();
-app.use(express.json());
+const app = express()
+app.use(express.json())
+app.use(cors())
+app.get('/', (req, res) => {
+  res.send('Hello')
+})
 
-app.get("/", (req, res) => {
-  res.send("Hello");
-});
-
-app.use("/api/v1", auth);
-app.use("/api/v2", todo);
-
+app.use('/api/v1', auth)
+app.use('/api/v2', todo)
+app.get('/', (req, res) => {
+  app.use(express.static(path.resolve(__dirname, 'frontend', 'build')))
+  res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+})
 app.listen(1000, () => {
-  console.log("server started");
-});
+  console.log('server started')
+})
--- server/db/connection.js
+++ server/db/connection.js
@@ -1,15 +1,13 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
 const connection = async (req, res) => {
   try {
-    await mongoose
-      .connect("mongodb+srv://jithu:jitzp%40123@cluster0.tyueh.mongodb.net/")
-      .then(() => {
-        console.log("Mongo connected");
-      });
+    await mongoose.connect('').then(() => {
+      console.log('Mongo connected')
+    })
   } catch (error) {
-    res.status(400).json({ message: "Not connected" });
+    res.status(400).json({ message: 'Not connected' })
   }
-};
+}
 
-connection();
+connection()
